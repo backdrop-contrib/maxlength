c8213a0e62a9aaae53ab0ca8569b59c7d105191c
Alex McFadyen <a_c_m@195063.no-reply.drupal.org>
2008-09-28 12:51:09 +0000

    commiting 2.x dev version

--- maxlength.js
+++ maxlength.js
@@ -1,14 +1,13 @@
 /* $Id$ */
 
-function maxlength_limit(textarea, limit) {
-  var remainingTag = document.getElementById('maxlength-counter-remaining');
-  var remainingCnt = limit - textarea.value.length;
+function maxlength_limit(input, limit) {
+  var remainingCnt = limit - input.value.length;
     
   if (remainingCnt < 0) {
-    textarea.value = textarea.value.substr(0, limit);
+    input.value = input.value.substr(0, limit);
         
     remainingCnt = 0;
   }
-    
-  remainingTag.innerHTML = remainingCnt;
+  
+  $('#' + $(input).attr('id') + '-maxlength-counter span.maxlength-counter-remaining').html(remainingCnt);
 }
