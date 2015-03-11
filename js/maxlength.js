2ba6a507aa0fbb217adc71dcb77e8c1a747b8929
barnettech <barnettech@gmail.com>
2015-03-11 15:09:53 -0400

    small change to the documentation for my previous commit 2331439

--- js/maxlength.js
+++ js/maxlength.js
@@ -323,9 +323,9 @@
   ml.tinymceChange = function(ed) {
     // CLone to avoid changing defaults
     var options = $.extend({}, ml.options[ed.editorId]);
-    // The following lines take the text from the wysiwyg and
-    // strip out special characters so an accurate character
-    // count can be taken.
+    /* The following lines take the text from the wysiwyg and
+      strip out some html and special characters so an accurate character
+      count can be taken. */
     var bodyContent = ml.tinymceGetData(ed);
     bodyContent = bodyContent.replace(/<\/?[^>]+(>|$)/g, "");
     bodyContent = bodyContent.replace(/\<p>/g, "");
